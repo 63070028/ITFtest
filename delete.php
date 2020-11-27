a3a7f493dafd083340e44e2e5fb355c201f5f59e
picnic <peekung602@gmail.com>
2020-11-27 10:04:21 +0700

    sdf

---
+++ delete.php
@@ -0,0 +1,21 @@
+<?php
+
+$conn = mysqli_init();
+mysqli_real_connect($conn, 'itf-test63070028.mysql.database.azure.com', 'it63070028@itf-test63070028', 'UWLtol53', 'itftest', 3306);
+if (mysqli_connect_errno($conn))
+{
+    die('Failed to connect to MySQL: '.mysqli_connect_error());
+}
+
+$id = $_GET['delete_id'];
+
+$sql = "DELETE FROM guestbook WHERE id = $id";
+
+if (mysqli_query($conn, $sql)) {
+    echo "New record created successfully";
+  } else {
+    echo "Error: " . $sql . "<br>" . mysqli_error($conn);
+  }
+  
+mysqli_close($conn);
+?>
